23ad7e9e05e4954caf596d067f7fb39d2cf24860
zhaoyingxu <949955482@qq.com>
2017-05-27 14:13:04 +0800

    划分服务完成,修改bug,还是需要再改angular,已与分服务之前的功能无二

--- cpa-service/src/main/java/cn/kanyun/cpa/service/unitexam/impl/UnitExamServiceImpl.java
+++ cpa-service/src/main/java/cn/kanyun/cpa/service/unitexam/impl/UnitExamServiceImpl.java
@@ -7,7 +7,7 @@ import cn.kanyun.cpa.model.exam.CpaOption;
 import cn.kanyun.cpa.model.exam.CpaRepertory;
 import cn.kanyun.cpa.service.CommonServiceImpl;
 import cn.kanyun.cpa.service.unitexam.IUnitExamService;
-import org.hibernate.service.Service;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.*;
