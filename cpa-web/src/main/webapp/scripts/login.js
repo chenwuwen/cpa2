f729dbd550d6a582b13b8997c622a5f24f1bd5a7
zhaoyingxu <949955482@qq.com>
2017-06-02 18:59:10 +0800

    准备架设dubbo和zookeeper,同时拟调用网易云音乐,拟加入redis做HIbernate二级缓存

--- cpa-web/src/main/webapp/scripts/login.js
+++ cpa-web/src/main/webapp/scripts/login.js
@@ -32,8 +32,8 @@ $(function () {
                         /*获取用户ID写入Cookie*/
                         var userId = result.data.id;
                         var Name = result.data.petName;
-                        addCookie("uid", userId, remember_me);//存储remember_me小时,此处实际设置的是Cookie的失效时间，正常情况下是关闭浏览器Cookie即失效
-                        addCookie("name", Name, remember_me);
+                        // addCookie("uid", userId, remember_me);//存储remember_me小时,此处实际设置的是Cookie的失效时间，正常情况下是关闭浏览器Cookie即失效
+                        // addCookie("name", Name, remember_me);
                         /*跳转到指定页面，window代表浏览器，location代表当前页面*/
                         window.location.href = "./page/main.html";
                     } else if (result.status == 2) {
