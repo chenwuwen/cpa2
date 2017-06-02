0c12c7c0b1cf84fa5510b54bf29e69fbfe012eb8
zhaoyingxu <949955482@qq.com>
2017-06-02 10:01:23 +0800

    准备架设dubbo和zookeeper,同时拟调用网易云音乐,拟加入redis做HIbernate二级缓存

--- cpa-dao/src/main/java/cn/kanyun/cpa/dao/CommonDaoImpl.java
+++ cpa-dao/src/main/java/cn/kanyun/cpa/dao/CommonDaoImpl.java
@@ -195,7 +195,7 @@ public abstract class CommonDaoImpl<K extends Serializable, T extends Serializab
         Session session = getSession();
         Query query = session.createQuery("select o from " + entityName + " o"
                 + whereql + buildOrderby(orderby));
-//        query.setCacheable(true);   //激活二级缓存查询
+        query.setCacheable(true);   //激活查询缓存,查询缓存,缓存的是对象的ID
         if (firstResult != -1 && maxResult != -1)
             query.setFirstResult(firstResult).setMaxResults(maxResult);
         setQueryParameter(query, params);
