ee9ee7c0cf888e1067cc2ddeb657a60899334ba8
zhaoyingxu <949955482@qq.com>
2017-07-06 11:09:04 +0800

    准备架设dubbo和zookeeper,同时拟调用网易云音乐,拟加入redis做HIbernate二级缓存

---
+++ cpa-web/src/main/java/cn/kanyun/cpa/util/WebUtil.java
@@ -0,0 +1,86 @@
+package cn.kanyun.cpa.util;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Rencb on 2016/5/16 0016.
+ */
+public class WebUtil {
+
+    /**
+     * 在session中获得user
+     * @param request
+     * @return
+     */
+//    public synchronized static UserSession getSessionUser(HttpServletRequest request){
+//        UserSession us  = (UserSession)request.getSession().getAttribute(ConstUtil.LOGIN_USER);
+//        return us;
+//    }
+//
+//    /**
+//     * 在session中获得user
+//     * @param request
+//     * @return
+//     */
+//    public synchronized static void setSessionUser(HttpServletRequest request, UserSession us){
+//        request.getSession().setAttribute(ConstUtil.LOGIN_USER,us);
+//    }
+
+    /**
+     * 验证当前的手机浏览器是否是app内置的浏览器
+     * @author zhb
+     * @date 2016年12月22日
+     * @param request
+     * @return
+     */
+    public static Boolean isBuiltInBrowser(HttpServletRequest request){
+        String userAgent = request.getHeader("user-agent").toLowerCase();
+        String regExpWeChat = "micromessenger";
+        String regExpWeBo = "weibo/";
+        String regExpQq = "qq/";
+        String regExUc="uc";
+
+        if(isMatchedStr(regExpQq,userAgent)){//是QQ内置的浏览器
+            return true;
+        }else if(isMatchedStr(regExpWeChat,userAgent)){//微信内置浏览器
+            return true;
+        }else if(isMatchedStr(regExpWeBo,userAgent)){//微博内置浏览器
+            return true;
+        }else if(isMatchedStr(regExUc, userAgent)){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isMatchedStr(String regex,String str){
+        Pattern p =Pattern.compile(regex,Pattern.MULTILINE);
+        String pattern = p.pattern();
+        Matcher m=p.matcher(str);
+        return m.find();
+    }
+    
+    /**
+     * 判断是否是手机端访问
+     * @param request
+     * @return
+     * @author ShanGuoFeng
+     * @date   2017年1月13日上午10:43:47
+     * @version v2.0
+     */
+    public static boolean isMobileDevice(HttpServletRequest request){
+        String requestHeader = request.getHeader("user-agent");
+        String[] deviceArray = new String[]{"android","mac os","windows phone"};
+        if(requestHeader == null)
+            return false;
+        requestHeader = requestHeader.toLowerCase();
+        for(int i=0;i<deviceArray.length;i++){
+            if(requestHeader.indexOf(deviceArray[i])>0){
+                return true;
+            }
+        }
+        return false;
+    }
+}
